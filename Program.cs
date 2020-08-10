d6f08eae62fb1786cc036bcd1527228ad99a8433
Andre <andre.foresti@ncr.com>
2020-08-10 13:00:50 -0300

    refatorado

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@
     using System.Linq;
     using System.Reflection;
 
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             foreach (var arquivo in Directory.EnumerateDirectories(".").ToList().SelectMany(dir => Directory.EnumerateFiles(dir, "Plugin.*.dll")))
             {
@@ -23,22 +23,12 @@
         {
             try
             {
-                var classe = Path.GetFileNameWithoutExtension(arquivo);
-                classe = classe.Replace("Plugin.", "");
-                classe = classe.Replace(".", "");
-                classe = $"Plugin{classe}.Plugin";
                 Console.WriteLine("*".PadRight(60, '*'));
-                var a = Assembly.LoadFrom(arquivo);
-                Console.WriteLine("- assembly carregado...");
-                var plugin = a.GetType(classe);
-                Console.WriteLine("- classe plugin encontrada...");
-                var obj = Activator.CreateInstance(plugin);
-                Console.WriteLine("- classe instanciada...");
-                var obterNome = plugin.GetMethod("ObterNome");
-                var obterVersao = plugin.GetMethod("ObterVersao");
-                Console.WriteLine("- metodos encontrados...");
-                var nome = obterNome?.Invoke(obj, null);
-                var versao = obterVersao?.Invoke(obj, null);
+                var assembly = CaregarAssembly(arquivo);
+                var plugin = ObterClassePlugin(arquivo, assembly);
+                var instancia = ObterInstancia(plugin);
+                var nome = ExecutarMetodo<string>("ObterNome", plugin, instancia);
+                var versao = ExecutarMetodo<string>("ObterVersao", plugin, instancia);
                 Console.WriteLine($" Plugin: {nome} ({versao}) carregado corretemente! ");
                 Console.WriteLine("*".PadRight(60, '*'));
             }
@@ -46,6 +36,36 @@
             {
                 Console.WriteLine(e);
             }
+        }
+
+        private static T ExecutarMetodo<T>(string metodo, Type plugin, object instancia)
+        {
+            var m = plugin.GetMethod(metodo);
+            return (T)m?.Invoke(instancia, null);
+
+        }
+
+        private static object ObterInstancia(Type plugin)
+        {
+            var obj = Activator.CreateInstance(plugin);
+            Console.WriteLine("- classe instanciada...");
+            return obj;
+        }
+
+        private static Type ObterClassePlugin(string arquivo, Assembly assembly)
+        {
+            var classe = Path.GetFileNameWithoutExtension(arquivo).Replace(".", "");
+            classe = $"{classe}.Plugin";
+            var plugin = assembly.GetType(classe);
+            Console.WriteLine("- classe plugin encontrada...");
+            return plugin;
+        }
+
+        private static Assembly CaregarAssembly(string arquivo)
+        {
+            var a = Assembly.LoadFrom(arquivo);
+            Console.WriteLine("- assembly carregado...");
+            return a;
         }
     }
 }
